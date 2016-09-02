06b0b076a83220ba4ba9b4fbdce09fe182db76ba
João Vitor Araujo <joao18araujo@gmail.com>
2016-09-02 00:17:36 -0300

    Creating structure for login and user signup

---
+++ app/src/main/java/com/time2desenho/wikalendario/UserActivity.java
@@ -0,0 +1,12 @@
+package com.time2desenho.wikalendario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
