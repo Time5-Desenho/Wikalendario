81438a23e2474684aa2d0769bbad72282fe8ab2d
Mateus Manuel <mateusmanuel21@hotmail.com>
2016-09-01 23:23:15 -0300

    Creating User Dao

---
+++ app/src/main/java/com/time2desenho/wikalendario/UserDao.java
@@ -0,0 +1,90 @@
+package com.time2desenho.wikalendario;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by mateus on 9/1/16.
+ */
+public class UserDao extends SQLiteOpenHelper {
+
+    public UserDao(Context context) {
+        super(context, "Wikalendario", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, username TEXT UNIQUE NOT NULL, email TEXT, password TEXT)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS Wikalendario";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+
+    public User findUser(User user) {
+        Cursor c = null;
+        User userFinded;
+
+        String sql = "SELECT * FROM Users WHERE id=?;";
+        SQLiteDatabase db = getReadableDatabase();
+
+        try {
+            c = db.rawQuery(sql, new String[] {user.getId().toString()});
+
+            userFinded = new User();
+
+            userFinded.setId(c.getLong(c.getColumnIndex("id")));
+            userFinded.setName(c.getString(c.getColumnIndex("name")));
+            userFinded.setUsername(c.getString(c.getColumnIndex("username")));
+            userFinded.setEmail(c.getString(c.getColumnIndex("email")));
+            userFinded.setPassword(c.getString(c.getColumnIndex("password")));
+        } finally {
+            c.close();
+        }
+
+        return userFinded;
+    }
+
+    public void insert(User user) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues data = getUserData(user);
+
+        db.insert("Users", null, data);
+    }
+
+    public void delete(User user) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        String[] params = {user.getId().toString()};
+        db.delete("Users", "id = ?", params);
+    }
+
+    public void update(User user) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues data = getUserData(user);
+
+        String[] params = {user.getId().toString()};
+        db.update("Users", data, "id = ?", params);
+    }
+
+    @NonNull
+    private ContentValues getUserData(User user) {
+        ContentValues data = new ContentValues();
+        data.put("name", user.getName());
+        data.put("username", user.getUsername());
+        data.put("email", user.getEmail());
+        data.put("password", user.getPassword());
+        return data;
+    }
+}
