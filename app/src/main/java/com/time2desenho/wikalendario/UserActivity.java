6a8f169b6d110cc9d2c8c22212b88ad81cca7bd4
Arthur Temporim <arthurrtl@gmail.com>
2016-09-02 00:23:14 -0300

    Adding integration to login and signup

--- app/src/main/java/com/time2desenho/wikalendario/UserActivity.java
+++ app/src/main/java/com/time2desenho/wikalendario/UserActivity.java
@@ -8,5 +8,6 @@ public class UserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
     }
 }
