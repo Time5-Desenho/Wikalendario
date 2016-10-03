995970473cc4a4c75d3b6dcfa54870dbc896ff33
João Vitor Araujo <joao18araujo@gmail.com>
2016-10-03 20:38:09 -0300

    Adding subjects activity
    Signed-off-by: Renata Soares <renata.sds09@gmail.com>
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>

--- app/src/main/java/com/time2desenho/wikalendario/SubjectsActivity.java
+++ app/src/main/java/com/time2desenho/wikalendario/SubjectsActivity.java
@@ -1,6 +1,7 @@
 package com.time2desenho.wikalendario;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
