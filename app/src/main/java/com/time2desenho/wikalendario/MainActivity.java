ef5be5f6a499b4f4ac1e4e85493cd1ec3e8bb152
arthurTemporim <arthurrtl@gmail.com>
2016-10-31 09:47:00 -0200

    Change view of project.

--- app/src/main/java/com/time2desenho/wikalendario/MainActivity.java
+++ app/src/main/java/com/time2desenho/wikalendario/MainActivity.java
@@ -111,14 +111,14 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.activity_event) {
             Intent intent2 = new Intent(this, EventActivity.class);
             startActivity(intent2);
-        } else if (id == R.id.nav_slideshow) {
+        }/* else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
