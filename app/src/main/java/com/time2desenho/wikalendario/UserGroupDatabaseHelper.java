0fdd6d9d464cc7fdccd38bf9bccf6f898bd92469
João Vitor Araujo <joao18araujo@gmail.com>
2016-11-02 11:58:47 -0200

    Adding User Group relation

---
+++ app/src/main/java/com/time2desenho/wikalendario/UserGroupDatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.time2desenho.wikalendario;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+public class UserGroupDatabaseHelper extends DatabaseHelper{
+    private Dao<UserGroup, Long> userGroupDAO;
+
+    public UserGroupDatabaseHelper(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
+        try {
+            TableUtils.createTableIfNotExists(connectionSource, UserGroup.class);
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource,
+                          int oldVersion, int newVersion) {
+        try {
+            TableUtils.dropTable(connectionSource, UserGroup.class, false);
+        }catch (java.sql.SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Dao<UserGroup, Long> getDAO() throws java.sql.SQLException {
+        if(userGroupDAO == null){
+            userGroupDAO = getDao(UserGroup.class);
+        }
+
+        return userGroupDAO;
+    }
+}
