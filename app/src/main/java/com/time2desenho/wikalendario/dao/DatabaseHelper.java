bf7de3b7edaab784e9d6374429be9f560127da0a
Mateus Manuel <mateusmanuel21@hotmail.com>
2016-11-08 21:01:44 -0200

    Adding jar to mysql connection and the async task to create tables

--- app/src/main/java/com/time2desenho/wikalendario/dao/DatabaseHelper.java
+++ app/src/main/java/com/time2desenho/wikalendario/dao/DatabaseHelper.java
@@ -2,13 +2,19 @@ package com.time2desenho.wikalendario.dao;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-import com.time2desenho.wikalendario.model.Class;
+
 import com.time2desenho.wikalendario.model.Event;
+import com.time2desenho.wikalendario.model.Class;
 import com.time2desenho.wikalendario.model.Group;
 import com.time2desenho.wikalendario.model.Subject;
 import com.time2desenho.wikalendario.model.User;
@@ -19,7 +25,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseHelper extends OrmLiteSqliteOpenHelper{
+public abstract class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     public static final String DATABASE_NAME = "sql3143440";
     public static final int DATABASE_VERSION = 1;
     public static final String HOST = "sql3.freemysqlhosting.net";
@@ -29,16 +35,12 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper{
 
 
     @Override
-    public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
+        public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
         try {
             connectionSource = new JdbcConnectionSource("jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD);
-            TableUtils.createTableIfNotExists(connectionSource, User.class);
-            TableUtils.createTableIfNotExists(connectionSource, Subject.class);
-            TableUtils.createTableIfNotExists(connectionSource, Class.class);
-            TableUtils.createTableIfNotExists(connectionSource, Group.class);
-            TableUtils.createTableIfNotExists(connectionSource, Event.class);
-            TableUtils.createTableIfNotExists(connectionSource, UserClass.class);
-            TableUtils.createTableIfNotExists(connectionSource, UserGroup.class);
+
+            new CreateTables().execute(connectionSource);
+
         } catch (java.sql.SQLException e) {
             e.printStackTrace();
         }
@@ -69,12 +71,41 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper{
     public Connection connectToDB() throws ClassNotFoundException,SQLException {
         Connection connection = null;
 
-        java.lang.Class.forName("com.mysql.jdbc.Driver");
+        try {
+            java.lang.Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
         connection = DriverManager.getConnection(
                 "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE_USERNAME,
                 DATABASE_USERNAME, DATABASE_PASSWORD);
 
         return connection;
+    }
+
+    private class CreateTables extends AsyncTask<ConnectionSource, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(ConnectionSource... connectionSource) {
+
+            try {
+                TableUtils.createTableIfNotExists(connectionSource[0], User.class);
+                TableUtils.createTableIfNotExists(connectionSource[0], Subject.class);
+                TableUtils.createTableIfNotExists(connectionSource[0], Class.class);
+                TableUtils.createTableIfNotExists(connectionSource[0], Group.class);
+                TableUtils.createTableIfNotExists(connectionSource[0], Event.class);
+                TableUtils.createTableIfNotExists(connectionSource[0], UserClass.class);
+                TableUtils.createTableIfNotExists(connectionSource[0], UserGroup.class);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+        @Override
+        protected void onPostExecute(Boolean bool) {
+            Log.d("ASYNC_TASK", "Created tables finished");
+        }
     }
 }
